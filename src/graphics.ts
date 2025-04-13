b3fdd2f2e30796f77d629e24a315c039ddc91124
Botolog <81357028+Botolog@users.noreply.github.com>
2025-04-13 13:19:16 +0300

    chats displayed proparly

--- src/graphics.ts
+++ src/graphics.ts
@@ -1,8 +1,9 @@
-import blessed, { Widgets } from "blessed";
-import { number, string } from "yargs";
+import blessed, { message, Widgets } from "blessed";
+import { hide, number, string } from "yargs";
 import { Cmnds, H } from "./Cmnd";
 import pkg from "whatsapp-web.js";
 import { client, READY, readyPromise } from "./wweb";
+import { version } from "node:process";
 
 // import blessed from "neo-blessed";
 // import * as blessed from 'neo-blessed';
@@ -14,75 +15,181 @@ var cmnds = new Cmnds()
 var screen: Widgets.Screen = blessed.screen({
     // dump: __dirname + '/logs/shadow.log',
     smartCSR: true,
-    title: "WhatClient 0.0.1"
+    title: "WhatClient 0.0.1 " + version
     // dockBorders: true,
     // warnings: true,
 });
 
-class Chats{
+class Chats {
     gui: GUI;
     element: Widgets.LayoutElement;
-    chats:Chat[] = [];
-    constructor(gui:GUI){
+    chats: Chat[] = [];
+    num2display: number = 10;
+    constructor(gui: GUI) {
         this.gui = gui
         this.element = blessed.layout({
-            parent: this.gui.display,
+            parent: this.gui.mainDisplay,
             top: 0,
             left: 0,
             // right: 0,
             width: "30%",
             // height: "100%",
-            border: "line",
+            // border: "line",
             style: {
                 bg: "green"
             },
             layout: "inline"
         })
-        client.getChats().then((chatsFound)=>{
+        client.getChats().then((chatsFound) => {
             chatsFound.forEach(chat => {
-                this.addChat(chat);
+                if (!chat.archived)
+                    this.addChat(chat);
             });
+            this.hideShow();
+            console.log("COOOOOOOOOOOOOL");
+
         })
-        // this.chats.push(new Chat(this, "ye"))
+
+        screen.on("resize", () => {
+            this.calcHideShow()
+            this.hideShow()
+        })
     }
 
-    addChat(chatStruct: pkg.Chat){
-        this.chats.push(new Chat(this, chatStruct))
+    calcHideShow() {
+        let h = this.element.height
+        let ch = this.chats[0].element.height
+        if (typeof (h) == "number" && typeof (ch) == "number")
+            this.num2display = Math.floor(h / ch) - 1
+    }
+
+    hideShow() {
+        // return
+        let allIndexes: number[] = [];
+        this.chats.forEach(ch => {
+            allIndexes.push(ch.Zindex)
+        });
+        allIndexes = allIndexes.sort((a, b) => a - b)
+        let indexes = allIndexes.slice(0, this.num2display + 1)
+        this.chats.forEach(ch => {
+            if (indexes.includes(ch.Zindex)) { ch.element.show() }
+            else { ch.element.hide() }
+        });
+        this.gui.inputBar.setContent(indexes.join(","))
+        screen.render()
+    }
+
+    addChat(chatStruct: pkg.Chat) {
+        let ch = new Chat(this, chatStruct)
+        ch.modIndex(this.chats.length)
+        this.chats.push(ch)
+    }
+
+    elevateChat(chat: Chat) {
+        // return
+        if (chat.chatStruct.pinned) return
+        this.chats.forEach(ch => {
+            if (!ch.chatStruct.pinned)
+                ch.modIndex(3)
+        });
+        chat.setIndex(3);
+        this.hideShow()
+        chat.updateIndicator();
+    }
+
+    findChat(name: string) {
+        return this.chats.find(chat => chat.chatStruct.name == name)
+    }
+
+    findChatIndex(name: string) {
+        return this.chats.findIndex(chat => chat.chatStruct.name == name)
+    }
+
+    matchChat(sriID: string) {
+        return this.chats.find(chat => chat.chatStruct.id._serialized == sriID)
+    }
+
+    matchChatIndex(sriID: string) {
+        return this.chats.findIndex(chat => chat.chatStruct.id._serialized === sriID)
     }
 }
 
-class Chat{
-    chatStruct:pkg.Chat;
+class Chat {
+    chatStruct: pkg.Chat;
     element: Widgets.BoxElement;
     manager: Chats;
-    constructor(manager:Chats, chatStruct: pkg.Chat){
+    indicator: Widgets.BoxElement;
+    Zindex: number = -1;
+    constructor(manager: Chats, chatStruct: pkg.Chat) {
         this.chatStruct = chatStruct;
         this.manager = manager;
-        
+
         this.element = blessed.box({
             parent: this.manager.element,
             // top: 0,
             left: 0,
             // bottom: 0,
             right: 0,
-            width: "90%",
+            width: "100%",
             height: 3,
             border: "line",
             content: H(chatStruct.name),
             style: {
                 bg: "red"
-            },            
+            },
         })
-        
+
+        this.indicator = blessed.box({
+            parent: this.element,
+            right: 1,
+            bottom: 0,
+            width: "shrink",
+            content: "1",
+            style: {
+                bg: "green"
+            },
+            hidden: true
+
+        })
+
+
+        if (chatStruct.pinned)
+            this.modIndex(-5)
+        //  this.element.index -= 10
+
+        this.updateIndicator()
+
         screen.render()
     }
+
+    setIndex(z: number) {
+        this.Zindex = z;
+        this.element.setIndex(this.Zindex)
+        this.updateIndicator();
+    }
+    modIndex(dz: number) {
+        this.Zindex += dz;
+        this.element.setIndex(this.Zindex)
+        this.updateIndicator();
+    }
+
+    updateIndicator() {
+        let msgnum = this.Zindex
+        // let msgnum = this.chatStruct.unreadCount
+        // if (msgnum <= 0) this.indicator.hide()
+        // else {
+        this.indicator.setContent(msgnum.toString())
+        this.indicator.show()
+        // }
+    }
+
 
 }
 
 class GUI {
-    
 
-    display: Widgets.BoxElement= blessed.box({
+
+    mainDisplay: Widgets.BoxElement = blessed.box({
         parent: screen,
         top: 0,
         left: 0,
@@ -93,16 +200,17 @@ class GUI {
         style: {
             bg: "yellow"
         }
-    
+
     });
 
+    inputBar: Widgets.BoxElement;
     input: Widgets.TextboxElement;
 
-    chats: Chats = new Chats(this);
+    normalChats: Chats = new Chats(this);
 
     constructor() {
-        
-        let inputBar = blessed.box({
+
+        this.inputBar = blessed.box({
             parent: screen,
             bottom: 0,
             left: 0,
@@ -114,15 +222,15 @@ class GUI {
             },
             mouse: true,
             keys: true,
-        
+
             input: true,
             focusable: true,
         })
         this.input = blessed.textbox({
-            parent: inputBar,
+            parent: this.inputBar,
             bottom: 0,
             right: 0,
-            width: "90%",
+            width: "30%",
             height: "shrink",
             border: "line",
             style: {
@@ -130,49 +238,62 @@ class GUI {
             },
             mouse: true,
             keys: true,
-        
+
             input: true,
             focusable: true,
             // clickable:true,
         })
-        
+
         this.input.on("focus", () => {
             this.input.readInput();
-        
+
         })
-        
+
         this.input.key("up", () => {
-            this.input.setValue(cmnds.before())
+            this.input.setValue(H(cmnds.before()))
             screen.render()
         })
         this.input.key("down", () => {
-            this.input.setValue(cmnds.after())
+            this.input.setValue(H(cmnds.after()))
             screen.render()
         })
-        
+
         this.input.key("return", async () => {
             cmnds.add(this.input.getText())
             this.input.clearValue()
             this.input.focus()
-        
+
             screen.render()
-        
+
         })
         this.input.focus()
         this.render()
     }
 
-    render(){
+    render() {
         screen.render();
     }
 }
 
-readyPromise.then(()=>{
+readyPromise.then(() => {
 
     let Gui = new GUI()
-    
-    setInterval(Gui.render, 500)
-    setTimeout(()=>{
+
+    client.on("message_create", async (message) => {
+        let chat = Gui.normalChats.matchChat((await message.getChat()).id._serialized)
+        if (chat != undefined)
+            Gui.normalChats.elevateChat(chat)
+    })
+
+    // setInterval(Gui.render, 500)
+    // client.getChats().then((chats)=>{
+    //     console.log(chats);
+
+    // }).catch((res)=>{
+    //     console.error(res);
+
+    // })
+    setTimeout(() => {
 
         // Gui.chats.chats.push(new Chat(Gui.chats))
         // Gui.chats.chats.push(new Chat(Gui.chats))
--- src/test.ts
+++ src/test.ts
@@ -5,58 +5,100 @@ var screen: Widgets.Screen = blessed.screen({
     // dump: __dirname + '/logs/shadow.log',
     smartCSR: true,
     title: "WhatClient 0.0.1",
-    autoPadding: true,
+    // autoPadding: true,
     // dockBorders: true,
     // warnings: true,
     
 });
 
-let big = blessed.layout({
+// let big = blessed.layout({
+//     parent: screen,
+//     right:0,
+//     top:0,
+//     height: "100%",
+//     width: "80%",
+//     layout: "inline",
+//     align: "center",
+//     border: "line",
+    
+
+// })
+
+// let o = blessed.box({
+//     parent: big,
+//     // top: 0,
+//     left: 0,
+//     // bottom: 0,
+//     right: 0,
+//     width: "half",
+//     shrink: false,
+//     height: 3,
+//     border: "line",
+//     content: "1",
+//     style: {
+//         bg: "red",
+//     },
+
+    
+    
+    
+// })
+
+// let t = blessed.box({
+//     parent: big,
+//     // top: 0,
+//     left: 0,
+//     // valign: "top",
+//     // bottom: 0,
+//     right: 0,
+//     width: "100%",
+//     height: 3,
+//     border: "line",
+//     content: "2",
+//     // style: {
+//     //     bg: "red"
+//     // },
+//     bg: "red"
+// })
+
+let g = blessed.box({
     parent: screen,
-    left:0,
-    top:0,
-    height: "100%",
+    left: 0,
+    bottom: 0,
     width: "100%",
-    layout: "inline",
-    align: "center",
+    height: 10,
     border: "line",
-
-})
-
-blessed.box({
-    parent: big,
-    // top: 0,
-    left: 0,
-    // bottom: 0,
-    right: 0,
-    width: "half",
-    shrink: false,
-    height: 3,
-    border: "line",
-    content: "1",
-    style: {
-        bg: "red",
-    },
-    
-    
-    
-})
-
-blessed.box({
-    parent: big,
-    // top: 0,
-    left: 0,
-    // valign: "top",
-    // bottom: 0,
-    right: 0,
-    width: "half",
-    height: 3,
-    border: "line",
-    content: "2",
-    style: {
-        bg: "red"
-    }
-    
+    bg: "yellow"
 })
 
 screen.render()
+// screen.
+screen.key('q', function() {
+    return screen.destroy();
+  });
+// screen.res
+
+// screen.on("render", (s)=>{
+//     console.log("ren");
+// })
+
+
+screen.on("resize", (s)=>{
+    // t.setContent(big.height.toString())
+    // console.log("res");
+    g.setContent(g.width.toString())
+
+})
+
+
+// setTimeout(()=>{
+
+//     t.setIndex(-18)
+//     t.style.bg = "blue";
+    
+//     screen.render()
+// }, 1000)
+
+setInterval(()=>{
+    
+}, 100)
